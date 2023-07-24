0e54b236f698f9b5d10e675a296088fb8a632036
jongheonleee <qwefghnm1212@gmail.com>
2023-07-24 19:56:09 +0900

    랜덤으로 명언이 나오는 페이지 만들기

---
+++ src/main/java/com/example/firstproject/controller/QuoteController.java
@@ -0,0 +1,30 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@Controller
+public class QuoteController {
+
+
+    @GetMapping("/quote")
+    public String getQuote(Model model) {
+        String[] quotes = {
+                "When you have faults, do not fear to abandon them.",
+                "They must often change who would be constant in happiness or wisdom.",
+                "Age is no guarantee of maturity.",
+                "Youth isn’t always all it’s touted to be",
+                "You will face many defeats in life, but never let yourself be defeated."
+        };
+
+
+        int idx = (int) (Math.random()* quotes.length);
+        model.addAttribute("quote", quotes[idx]);
+
+        return "quote";
+    }
+}
