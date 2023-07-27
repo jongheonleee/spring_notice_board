3ce39b7014315213c6030d957371f9ad4b43e374
jongheonleee <qwefghnm1212@gmail.com>
2023-07-27 12:22:33 +0900

    회원 정보 간의 페이지 이동 구현

--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
