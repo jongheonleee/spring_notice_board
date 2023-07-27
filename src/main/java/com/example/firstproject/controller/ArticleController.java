c206a473f16d2f08aa09e36a83ead619dd30d546
jongheonleee <qwefghnm1212@gmail.com>
2023-07-27 15:02:24 +0900

    게시글 수정하기- (1) 데이터 읽어오기 & (2) 데이터 업데이트

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -77,4 +77,24 @@ public class ArticleController {
         // 3. 수정 페이지 반환
         return "articles/edit";
     }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form, Model model) {
+        log.info(form.toString());
+        // 1. DTO -> Entity
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. Entity DB에 저장
+        // 2-1. DB에서 기존 데이터 가져옴
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2. 기존 데이터 값을 갱신
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
