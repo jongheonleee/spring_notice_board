08e5941e84f76e1632183c73b2f86b502b4a7a47
jongheonleee <qwefghnm1212@gmail.com>
2023-07-31 18:17:34 +0900

    간단한 sql문 작성 및 rest api 실습

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(id, title, content) VALUES(1, 'aaaa', '1111');
-INSERT INTO article(id, title, content) VALUES(2, 'bbbb', '2222');
-INSERT INTO article(id, title, content) VALUES(3, 'cccc', '3333');
+INSERT INTO article(title, content) VALUES('aaaa', '1111');
+INSERT INTO article(title, content) VALUES('bbbb', '2222');
+INSERT INTO article(title, content) VALUES('cccc', '3333');
 
 INSERT INTO member(id, email, password, name, age) VALUES(1, 'qwe1313@gmail.com', '1111', '여늘', 24);
 INSERT INTO member(id, email, password, name, age) VALUES(2, 'qwe1212@gmail.com', '2222', '연서', 26);
