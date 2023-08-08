5fd9ee760f7c3e57fa537e500bfaed12d5f493a9
jongheonleee <qwefghnm1212@gmail.com>
2023-08-08 16:43:20 +0900

    ~~

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,4 +15,20 @@ INSERT INTO bread(name, price) VALUES('소보루', '2800');
 INSERT INTO bread(name, price) VALUES('모카빵', '3100');
 
 
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 ㄱ');
+INSERT INTO article(title, content) VALUES('당신의 드림카는?', '댓글 ㄲ');
+INSERT INTO article(title, content) VALUES('당신의 특기는?', '댓글 ㄱㄱㄱ');
+
+
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'lee', '헝거게임');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'jay', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'park', '메이즈러너');
+
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'lee', '맥라렌');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'jay', '벤츠');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'park', '아우디');
+
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'lee', '알고리즘 문제 풀이');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'jay', '헬스');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'park', '미술');
 
---
+++ src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -0,0 +1,88 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    @DisplayName("테스트_결과에_보여_줄_이름")
+    void findByArticleId() {
+        /* case 1 : 4번 게시글의 모든 댓글 조회 */
+        {
+            // 1. 입력 데이터 준비
+            Long articleId = 4L;
+
+            // 2. 실제 데이터
+            List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+            // 3. 예상 데이터
+            Article article = new Article(4L, "당신의 인생 영화는?", "댓글 ㄱ");
+            Comment a = new Comment(1L, article, "lee", "헝거게임");
+            Comment b = new Comment(2L, article, "jay", "굿 윌 헌팅");
+            Comment c = new Comment(3L, article, "park", "메이즈러너");
+            List<Comment> expected = Arrays.asList(a, b, c);
+
+
+            // 4. 비교 및 검증
+            assertEquals(expected.toString(), comments.toString(), "4번 글의 모든 댓글을 출력!");
+
+        }
+
+        /* case 2 : 1번 게시글의 모든 댓글 조회 */
+        {
+            // 1. 입력 데이터 준비
+            Long articleId = 1L;
+
+            // 2. 실제 데이터
+            List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+            // 3. 예상 데이터
+            Article article = new Article(1L, "aaaa", "1111");
+            List<Comment> expected = Arrays.asList();
+
+            // 4. 비교 및 검증
+            assertEquals(expected.toString(), comments.toString(), "1번 글은 댓글이 없음");
+
+        }
+    }
+
+    @Test
+    @DisplayName("특정 닉네임의 모든 댓글 조회")
+    void findByNickname() {
+        /* case 1 : "park"의 모든 댓글 조회 */
+        {
+            // 1. 입력 데이터 준비
+            String nickname = "park";
+
+            // 2. 실제 데이터
+            List<Comment> comments = commentRepository.findByNickname(nickname);
+
+            // 3. 예상 데이터
+            Comment a = new Comment(3L, new Article(4L, "당신의 인생 영화는?", "댓글 ㄱ"), nickname, "메이즈러너");
+            Comment b = new Comment(6L, new Article(5L, "당신의 드림카는?", "댓글 ㄲ"), nickname, "아우디");
+            Comment c = new Comment(9L, new Article(6L, "당신의 특기는?", "댓글 ㄱㄱㄱ"), nickname, "미술");
+            List<Comment> expected = Arrays.asList(a, b, c);
+
+            // 4. 비교 및 검증
+            assertEquals(expected.toString(), comments.toString(), "park의 모든 댓글을 출력!");
+
+        }
+    }
+
+
+
+}
